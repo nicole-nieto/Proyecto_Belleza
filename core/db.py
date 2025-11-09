f2f48d385a4bd2feda10c38e019845a1a1fac858
Nicole <nicolnieto48@gmail.com>
2025-11-09 15:56:36 -0500

    :(

--- core/db.py
+++ core/db.py
@@ -2,15 +2,9 @@ import os
 from sqlmodel import SQLModel, create_engine, Session
 from dotenv import load_dotenv
 
-# Cargar variables del archivo .env
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    # Si no hay variable, usa SQLite por defecto (solo para emergencia)
-    DATABASE_URL = "sqlite:///belleza.db"
-
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
